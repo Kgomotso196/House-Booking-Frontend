81007730ec6d83b0b2f25bb7d89c9021872c4cb8
tonnytech <tonnytei4@gmail.com>
2023-10-28 09:58:22 +0300

    install redux and dependencies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import logo from './logo.svg';
+// eslint-disable
+
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import fetchReservations from './services/reservationService';
+import fetchHouses from './services/housesService';
 
 function App() {
+  const Reservations = useSelector((state) => state.reservations.reservations);
+  const Houses = useSelector((state) => state.houses.houses);
+  console.log(Houses);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchReservations());
+    dispatch(fetchHouses());
+  }, [dispatch]);
+
+  let reserveData;
+  let houseData;
+
+  // display houses
+
+  if (Houses.length === 0) {
+    houseData = <p>No house data</p>;
+  } else {
+    Houses.map((data) => {
+      houseData = <p>{data.house_name}</p>;
+      return houseData;
+    });
+  }
+
+  // display reservations
+
+  if (Reservations.lenght === 0) {
+    reserveData = <p>No reservation Data</p>;
+  }
+  Reservations.map((data) => {
+    reserveData = <p key={data.checking_date}>{data.checking_date}</p>;
+    return reserveData;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data appears here</h1>
+      <div>
+        <strong>From reservationSlice:</strong>
+        {reserveData}
+      </div>
+      <div>
+        <strong>From housesSlice:</strong>
+        {houseData}
+      </div>
     </div>
   );
 }
