ce32f444b9ef2738535656234c345ecdac04e48f
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-10-30 18:37:26 +0200

    Implement required changes

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -14,12 +14,12 @@ const NavMenu = () => {
       <nav className={`col-4 col-md-2 flex-column ${sidebar ? 'open' : ''}`}>
         <p className="d-flex justify-content-end p-2"><i className="fa-solid fa-x text-light" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav" /></p>
         <img className="nav-logo" src={navlogo} width={140} height={40} alt="Navbar logo" />
-        <ul className=" d-flex flex-column fs-6 pt-4">
-          <li className="nav-item-list"><NavLink to="/" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">Home</p></NavLink></li>
-          <li className="nav-item-list"><NavLink to="/add-house" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">Add House</p></NavLink></li>
-          <li className="nav-item-list"><NavLink to="/Delete-house" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">Delete House</p></NavLink></li>
-          <li className="nav-item-list"><NavLink to="/add-reservation" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">Add Reservation</p></NavLink></li>
-          <li className="nav-item-list"><NavLink to="/reservations" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">Reservations</p></NavLink></li>
+        <ul className="nav-items d-flex flex-column fs-6 pt-4">
+          <li className="nav-item-list"><NavLink to="/" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">HOME</p></NavLink></li>
+          <li className="nav-item-list"><NavLink to="/add-house" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">ADD HOUSE</p></NavLink></li>
+          <li className="nav-item-list"><NavLink to="/Delete-house" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">DELETE HOUSE</p></NavLink></li>
+          <li className="nav-item-list"><NavLink to="/add-reservation" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">ADD RESERVATION</p></NavLink></li>
+          <li className="nav-item-list"><NavLink to="/reservations" activeClassName="active" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav"><p className="tab">RESERVATIONS</p></NavLink></li>
         </ul>
         <footer className="d-flex flex-column">
           <div className="socials d-flex flex-row" role="button" tabIndex={0} onClick={openMenu} onKeyDown={openMenu} aria-label="toggle nav">
