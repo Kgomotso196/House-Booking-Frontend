1f4bbc3947126b608010d140ac96f996b78927db
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-10-30 13:56:20 +0200

    Implement NavMenu and integrate components in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-// eslint-disable
-
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import NavMenu from './components/NavMenu/NavMenu';
 import fetchReservations from './services/reservationService';
 import fetchHouses from './services/housesService';
+import Home from './components/Home';
+import AddHouse from './components/AddHouse';
+import DeleteHouse from './components/DeleteHouse';
+import AddReservation from './components/AddReservation';
+import Reservations from './components/Reservations';
 
 function App() {
-  const Reservations = useSelector((state) => state.reservations.reservations);
-  const Houses = useSelector((state) => state.houses.houses);
-  console.log(Houses);
+  const ReservationsData = useSelector((state) => state.reservations.reservations);
+  const HousesData = useSelector((state) => state.houses.houses);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -22,37 +25,44 @@ function App() {
 
   // display houses
 
-  if (Houses.length === 0) {
+  if (HousesData.length === 0) {
     houseData = <p>No house data</p>;
   } else {
-    Houses.map((data) => {
-      houseData = <p>{data.house_name}</p>;
-      return houseData;
-    });
+    houseData = HousesData.map((data) => (
+      <p key={data.id}>{data.house_name}</p>
+    ));
   }
 
   // display reservations
 
-  if (Reservations.lenght === 0) {
+  if (ReservationsData.length === 0) {
     reserveData = <p>No reservation Data</p>;
+  } else {
+    reserveData = ReservationsData.map((data) => (
+      <p key={data.checking_date}>{data.checking_date}</p>
+    ));
   }
-  Reservations.map((data) => {
-    reserveData = <p key={data.checking_date}>{data.checking_date}</p>;
-    return reserveData;
-  });
 
   return (
-    <div className="App">
-      <h1>Data appears here</h1>
-      <div>
-        <strong>From reservationSlice:</strong>
-        {reserveData}
+    <>
+      <NavMenu />
+      <div className="App">
+        <h1>Data appears here</h1>
+        <Home />
+        <AddHouse />
+        <DeleteHouse />
+        <AddReservation />
+        <Reservations />
+        <div>
+          <strong>From reservationSlice:</strong>
+          {reserveData}
+        </div>
+        <div>
+          <strong>From housesSlice:</strong>
+          {houseData}
+        </div>
       </div>
-      <div>
-        <strong>From housesSlice:</strong>
-        {houseData}
-      </div>
-    </div>
+    </>
   );
 }
 
