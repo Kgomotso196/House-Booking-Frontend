2d0a2e7b7a2e7c0548b3ec3311c4a4359b18ce63
tjay1760 <mcjthiongo@gmail.com>
2023-10-31 13:54:14 +0300

    Create house details boilerplate

--- src/components/HouseDetails/HouseDetails.js
+++ src/components/HouseDetails/HouseDetails.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './HouseDetails.css';
 
 const HouseDetails = () => (
-  <div className="house-details">HouseDetails</div>
+  <div className="house-details">
+    <div className="details">
+      <img src="https://media.istockphoto.com/id/1448386210/photo/a-large-gray-craftsman-new-construction-house-with-a-landscaped-yard-and-leading-pathway.webp?s=1024x1024&w=is&k=20&c=zr2ApXoNwve2CoVj3GfyaWU06ELZV4EzST6nuyOvrqU=" alt="House" />
+      <h1>House Name: </h1>
+      <p>Location</p>
+      <p>Description</p>
+
+    </div>
+  </div>
 );
 export default HouseDetails;
