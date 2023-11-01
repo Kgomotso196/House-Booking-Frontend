734f54744119ccceebc512871382e724af566799
tonnytech <tonnytei4@gmail.com>
2023-11-01 15:08:00 +0300

    Add postHousesSlice and postReservationSlice

--- src/redux/slices/PostHousesSlice.js
+++ src/redux/slices/PostHousesSlice.js
@@ -1,2 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 import houseServiceAPI from '../../services/housesService';
+
+const postHousesSlice = createSlice({
+    name: 'studio',
+    initialState,
+    reducers: {
+        resetSuccessful: (state) => ({ ...state, isSuccessfull: false }),
+    },
+    extraReducers: (builder) => {
+      
+      // create a new studio
+      builder
+            .addCase(createStudio.pending, (state) => ({
+        ...state,
+        status: 'loading',
+        isSuccessful: false,
+      }))
+            .addCase(createStudio.fulfilled, (state) => ({
+        ...state,
+        status: 'successful',
+        isSuccessful: true,
+      }))
+            .addCase(createStudio.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+        isSuccessful: false,
+      }));
+    },
+  });
+  
+  export const {resetSuccessful  } = postHousesSlice.actions;
+  export default postHousesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import getReservationSlice from './slices/GetReservationSlice';
 import getHosesSlice from './slices/GetHosesSlice';
+import postReservationSlice from './slices/PostReservationSlice';
 
 const store = configureStore({
   reducer: {
     reservations: getReservationSlice,
     houses: getHosesSlice,
+    postReservations: postReservationSlice,
   },
 });
 
