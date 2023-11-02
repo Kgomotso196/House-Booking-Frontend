2fc8c9318b33fe73a07a0cfcefe261600f6fc46c
tjay1760 <mcjthiongo@gmail.com>
2023-11-02 11:54:07 +0300

    Add house details

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavMenu from './components/NavMenu/NavMenu';
-import Home from './components/Home';
+// import Home from './components/Home';
 import AddHouse from './components/Addhouse/AddHouse';
 import DeleteHouse from './components/DeleteHouse';
 import AddReservation from './components/AddReservation';
 import Reservations from './components/Reservations';
+import HouseDetails from './components/HouseDetails/HouseDetails';
 
 function App() {
   return (
     <>
       <NavMenu />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HouseDetails />} />
         <Route path="/add-house" element={<AddHouse />} />
         <Route path="/delete-house" element={<DeleteHouse />} />
         <Route path="/add-reservation" element={<AddReservation />} />
