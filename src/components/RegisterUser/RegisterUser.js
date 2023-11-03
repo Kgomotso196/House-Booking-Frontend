52e5a61088df4e7d8a66eed400773db09b8b9561
tonnytech <tonnytei4@gmail.com>
2023-11-03 09:19:42 +0300

    Added register user form

---
+++ src/components/RegisterUser/RegisterUser.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const RegisterUser = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e)
+    }
+
+    const handleChange = (e) => {
+        console.log(e)
+    };
+
+  return (
+    <div>   <form className=" " onSubmit={handleSubmit}>
+        <label htmlFor="UserName" className="">
+          User Name
+          <input
+            type="text"
+            name="userName"
+            value="user Name value"
+            onChange={handleChange}
+            id="UserName"
+            className=""
+            placeholder="Enter House Name"
+          />
+        </label>
+
+        <label htmlFor="userEmail" className=" ">
+          User Email
+          <input
+            type="email"
+            name="email"
+            value="userEmail"
+            onChange={handleChange}
+            id="UserEmail"
+            className=""
+            placeholder="Enter Your Email"
+          />
+        </label>
+
+        <label htmlFor="UserPassword" className="">
+          Password
+          <input
+            type="text"
+            name="Password"
+            value="Password"
+            onChange={handleChange}
+            id="UserPassword"
+            className=""
+            placeholder="Enter Your Password"
+          />
+        </label>
+
+        <label htmlFor="UserPasswordConfirmation" className="">
+          Password Confirmation
+          <input
+            type="text"
+            name="PasswordConfirmation"
+            value="UserPasswordConfirmation"
+            onChange={handleChange}
+            id="UserPasswordConfirmation"
+            className=""
+            placeholder="Confirm Your password"
+          />
+        </label>
+        <div>
+          <input type="submit" className="" value="" />
+        </div>
+      </form></div>
+  )
+}
+
+export default RegisterUser
