e1929937d181cfe0d2803f666a466ecbb10a668b
Derrick1451 <haylederrick14@gmail.com>
2023-11-06 15:45:21 +0300

    Update addreservation file

--- src/components/AddReservation.js
+++ src/components/AddReservation.js
@@ -1,45 +1,58 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import reservationServiceAPI from '../services/reservationService'
+import { useNavigate } from 'react-router-dom';
 
 const AddReservation = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     checking_date: '',
     checkout_date: '',
     city: '',
+    house_price:'',
     // Add other fields as per your reservation model
   });
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('http://3001/api/v1/reservations', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      if (response.ok) {
-        // Redirecting user
-      } else {
-        // Handling error
-      }
-    } catch (error) {
-      // Handling error
-    }
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
   };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      const updatedReservationData = {
+        checking_date: formData.checking_date,
+        checkout_date: formData.checkout_date,
+        city: formData.city,
+        user_id: 1, // Include the user_id here
+        house_id: 1,
+        house_price:formData.house_price,
+      };
+      dispatch(reservationServiceAPI.addReservation(updatedReservationData))
+      navigate("/reservations")
+      setFormData('')
   };
-
+   
   return (
     <div>
       <h1>Add Reservation</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="checking_date" value={formData.checking_date} onChange={handleChange} placeholder="Checking Date" />
-        {/* Reservation details will come here */}
-        <button type="submit">Add Reservation</button>
+      <label>
+         Checking Date: <input type="text" name='checking_date' value={formData.checking_date} onChange={handleChange} />
+        </label>
+        <label>
+        Checkingout Date: <input type="text" name='checkout_date' value={formData.checkout_date} onChange={handleChange} />
+        </label>
+        <label>
+        City: <input type="text" name='city' value={formData.city} onChange={handleChange} />
+        </label>
+        <label>
+        House Price: <input type="text" name='house_price' value={formData.house_price} onChange={handleChange} />
+        </label>
+
+
+        <button type="submit" onClick={handleSubmit}>Add Reservation</button>
       </form>
     </div>
   );
