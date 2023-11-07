cf4e3db8bf89b8131adbdf535535c3d03e9f2e3f
tjay1760 <mcjthiongo@gmail.com>
2023-11-07 13:11:01 +0300

    update house details

--- src/components/HouseDetails/HouseDetails.js
+++ src/components/HouseDetails/HouseDetails.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { UseSelector } from 'react-redux/es/hooks/useSelector';
-// import { useParams } from 'react-router-dom';
 import './HouseDetails.css';
 import Table from 'react-bootstrap/Table';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight, faGear } from '@fortawesome/free-solid-svg-icons'; // Import the specific icons you need
+import Wheel from '../../assets/Wheel.png'; // Import the image
 
 const HouseDetails = () => (
   <div className="house-details">
+    <img src="https://media.istockphoto.com/id/1448386210/photo/a-large-gray-craftsman-new-construction-house-with-a-landscaped-yard-and-leading-pathway.webp?s=1024x1024&w=is&k=20&c=zr2ApXoNwve2CoVj3GfyaWU06ELZV4EzST6nuyOvrqU=" alt="House" />
     <div className="details">
-      <img src="https://media.istockphoto.com/id/1448386210/photo/a-large-gray-craftsman-new-construction-house-with-a-landscaped-yard-and-leading-pathway.webp?s=1024x1024&w=is&k=20&c=zr2ApXoNwve2CoVj3GfyaWU06ELZV4EzST6nuyOvrqU=" alt="House" />
       <h2>House</h2>
       <Table striped responsive size="sm">
         <thead />
@@ -21,13 +22,26 @@ const HouseDetails = () => (
             <td>Description text goes here.</td>
           </tr>
         </tbody>
-
       </Table>
+      <p>
+        <strong>5% Discount</strong>
+        {' '}
+        for new users
+      </p>
+      <p className="others">
+        View other houses&nbsp;&raquo;
+        <img src={Wheel} alt="Wheel" />
+      </p>
 
-      {/* <div>
-        <button type="button" className="reserve" onClick={console.log('reserve')}>Reserve</button>
-      </div> */}
+      <div>
+        <button type="button" className="reserve" onClick={() => console.log('reserve')}>
+          <FontAwesomeIcon icon={faGear} />
+          Reserve
+          <FontAwesomeIcon icon={faChevronCircleRight} />
+        </button>
+      </div>
     </div>
   </div>
 );
+
 export default HouseDetails;
