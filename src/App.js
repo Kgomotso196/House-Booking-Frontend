d29f7ea038eeb8dc55596402ac9c287c9e1b1e6d
tjay1760 <mcjthiongo@gmail.com>
2023-11-07 12:29:20 +0300

    Add route to House details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddHouse from './components/Addhouse/AddHouse';
 import DeleteHouse from './components/DeleteHouse';
 import AddReservation from './components/AddReservation';
 import Reservations from './components/Reservations';
+import HouseDetails from './components/HouseDetails/HouseDetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NavMenu />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/house-details" element={<HouseDetails />} />
         <Route path="/add-house" element={<AddHouse />} />
         <Route path="/delete-house" element={<DeleteHouse />} />
         <Route path="/add-reservation" element={<AddReservation />} />
