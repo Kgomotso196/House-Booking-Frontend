8a3af0dc2bfce144a5251642651fe3c75e430fb7
tjay1760 <mcjthiongo@gmail.com>
2023-11-08 22:34:12 +0300

    Add test file

--- src/__tests__/components.test.js
+++ src/__tests__/components.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Home from '../components/Home/Home';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import AddHouse from '../components/Addhouse/AddHouse';
 
-test('renders Home component', () => {
-  const { asFragment } = render(<Home />);
+// Mock reducer to mimic your Redux slice
+const mockHousesReducer = (state = { houses: [] }, action) => {
+  // Define how your reducer should handle actions
+  switch (action.type) {
+    case 'houses/addHouse':
+      return {
+        ...state,
+        houses: [...state.houses, action.payload],
+      };
+    // Add cases for other actions as needed
+    default:
+      return state;
+  }
+};
+
+test('Should match the snapshot of AddHouse component', () => {
+  const store = configureStore({
+    reducer: {
+      houses: mockHousesReducer,
+    },
+  });
+
+  const { asFragment } = render(
+    <Provider store={store}>
+      <AddHouse />
+    </Provider>,
+  );
+
   expect(asFragment()).toMatchSnapshot();
 });
