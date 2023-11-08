21b3f57b633d5aef63a774d913c1957028dd5f69
Derrick1451 <haylederrick14@gmail.com>
2023-11-08 10:19:40 +0300

    Modify reservation files

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,9 +1,8 @@
 export const baseUrl = 'http://localhost:3000';
 
 export const getLocalStorate = () => {
-  console.log('Get the local storage');
+
 };
 
 export const setLocalStorage = () => {
-  console.log('set local storage');
 };
