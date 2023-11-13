e6a7d01474345128a053ad648b235bdc165e7938
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-11-13 18:43:39 +0200

    Include backend URL

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://127.0.0.1:3000';
+export const baseUrl = 'https://houseboooking0.onrender.com/';
 
 export const getLocalStorage = () => {
   const userToken = localStorage.getItem('user');
